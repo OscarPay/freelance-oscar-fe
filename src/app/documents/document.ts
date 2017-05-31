aeddd02ad385f554e8041efefc05edd0167afcdd
Oscar <nike-o_94@hotmail.com>
2017-05-31 14:13:38 -0500

    Added document interface and array of documents showing in the documents component

---
+++ src/app/documents/document.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by oscar on 31/05/17.
+ */
+export interface Document {
+  title: string;
+  description: string;
+  file_url: string;
+  updated_at: string;
+  image_url: string;
+}
---
+++ src/app/documents/documents.component.ts
@@ -0,0 +1,35 @@
+/**
+ * Created by oscar on 31/05/17.
+ */
+import {Component} from '@angular/core';
+import {Document} from './document';
+
+@Component({
+  selector: 'documents',
+  templateUrl: 'documents.component.html'
+})
+export class DocumentsComponent {
+  documents: Document[] = [
+    {
+      title: 'My First Doc',
+      description: 'asdasd asdasd',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    },
+    {
+      title: 'My Second Doc',
+      description: 'asdasd asdasd',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    },
+    {
+      title: 'My Last Doc',
+      description: 'asdasd asdasd',
+      file_url: 'http://google.com',
+      updated_at: '11/11/16',
+      image_url: 'http://google.com'
+    }
+  ];
+}
