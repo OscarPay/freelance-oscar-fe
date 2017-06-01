9d810124495b66294b5d49f5055e4177811a3c91
Oscar <nike-o_94@hotmail.com>
2017-06-01 15:46:22 -0500

    Implemented show proposal

---
+++ src/app/proposal/proposal-show.component.ts
@@ -0,0 +1,28 @@
+/**
+ * Created by oscar on 1/06/17.
+ */
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from '@angular/router';
+import {Proposal} from './proposal';
+
+@Component({
+  selector: 'proposal-show',
+  templateUrl: 'proposal-show.component.html'
+})
+export class ProposalShowComponent implements OnInit {
+  id: number;
+  routedId: any;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {};
+
+  ngOnInit(): void {
+    this.routedId = this.route.params.subscribe(
+      params => {
+        this.id = +params['id'];
+      }
+    );
+  };
+
+}
