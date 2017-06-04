9d6ef6b574f26707b4aac198e52097f80803823d
Oscar <nike-o_94@hotmail.com>
2017-06-04 09:24:40 -0500

    Implemented proposal creation

--- src/app/proposal/proposal-new.component.ts
+++ src/app/proposal/proposal-new.component.ts
@@ -2,13 +2,35 @@
  * Created by oscar on 31/05/17.
  */
 import {Component} from '@angular/core';
+import {Observable} from 'rxjs/Rx';
+
+import {ProposalService} from './proposal.service';
 import {Proposal} from './proposal';
 
 @Component({
   selector: 'proposal-new',
   templateUrl: 'proposal-new.component.html',
-  styleUrls: ['proposal-new.component.css']
+  styleUrls: ['proposal-new.component.css'],
+  providers: [ProposalService]
 })
 export class ProposalNewComponent {
   proposal = new Proposal();
+  submitted = false;
+
+  constructor(private proposalService: ProposalService) {
+  }
+
+  createProposal(proposal) {
+    this.submitted = true;
+    this.proposalService.createProposal(proposal)
+      .subscribe(
+        data => {
+          return true;
+        },
+        error => {
+          console.log('Error saving proposal');
+          return Observable.throw(error);
+        }
+      );
+  }
 }
